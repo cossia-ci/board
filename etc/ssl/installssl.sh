c4814e75575da9aee386944d904c6aeb82c3656c
cossia <cossia@naver.com>
2022-04-07 15:34:42 +0900

    null

--- etc/ssl/installssl.sh
+++ etc/ssl/installssl.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 source ../shell/env.sh
+SHELL_PATH=`pwd -P`
 
 cp -f ../../etc/shell/after.conf ../../etc/nginx/default.conf 
 sed -i s/-EMAIL-/$EMAIL/g ./docker-compose.yml
 
 docker-compose up --no-deps fcertbot
 docker rm Certbot
+
+crontab -e 0 0 1 * * $SHELL_PATH/ssh.sh
