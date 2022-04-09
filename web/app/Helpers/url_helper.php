e95208c63cd21f09f3655b8afd002252ee3e1b47
cossia <cossia@naver.com>
2022-04-09 17:20:48 +0900

    get_count_table

--- web/app/Helpers/url_helper.php
+++ web/app/Helpers/url_helper.php
@@ -171,8 +171,10 @@ if(! function_exists('get_count_table')){
     function get_count_table( string $table ) : int
 	{
 		$db = db_connect();
-		$builder = $db->table($table)->selectCount('ano')->where('`delDt` IS NULL', null, false);
-		$data = $builder->get()->getResult('array')[0];
+		if( $this->db->tableExists($table) ){
+			$builder = $db->table($table)->selectCount('ano')->where('`delDt` IS NULL', null, false);
+			$data = $builder->get()->getResult('array')[0];
+		}
 		return $data['ano']??0;
 	}
 }
