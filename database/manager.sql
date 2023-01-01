27c082dbe298256346b7a19ad7e4f0fd87eb7e65
cossia <cossia@naver.com>
2023-01-01 11:41:20 +0900

    2023-01-01

--- database/manager.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `co_manager` (`email`,`password`,`level`,`info`,`regDt`)
-VALUES
-('super@user.com', '[비밀번호]', 99, '{"name":"최고관리자"}', NOW() ),
-('admin@user.com', '[비밀번호]', 10, '{"name":"관리자"}', NOW() )
